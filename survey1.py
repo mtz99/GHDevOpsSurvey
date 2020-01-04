968ec712cb2bbcb7e50c0d4569de8f3f4478b8c1
Matthew Zhang <mtz99@users.noreply.github.com>
2020-01-04 10:37:05 -0800

    Added string checking

--- survey1.py
+++ survey1.py
@@ -1,16 +1,16 @@
 import re
 
 def main():
-    fname = input(str("Enter your first name:"))
-    lname = input(str("Enter your last name:"))
+    fname = getFname()
+    lname = getLname()
     dob = getDob()
-    email = input(str("Enter your email:"))
+    email = getEmail()
     zcode = getZcode()
-    city = input(str("Enter your city:"))
-    state = input(str("Enter your state:"))
-    race = input(str("Enter your race:"))
+    city = getCity()
+    state = getState()
+    race = getRace()
     age = getAge()
-    sex = input(str("Enter your sex:"))
+    sex = getSex()
 
     print(fname)
     print(lname)
@@ -23,6 +23,23 @@ def main():
     print(age)
     print(sex)
 
+def getFname():
+    while True:
+        x = input("Enter your first name:")
+        if re.match("[a-zA-Z]+$", x):
+            return x
+        else:
+            print("Please enter it again:")
+
+def getLname():
+    while True:
+        x = input("Enter your last name:")
+        if re.match("[a-zA-Z]+$", x):
+            return x
+        else:
+            print("Please enter it again:")
+
+
 def getDob():
     while True:
         x = input("Enter your date of birth (mm/dd/yy):")
@@ -30,6 +47,15 @@ def getDob():
             return x
         else:
             print("Please enter it again:")
+
+def getEmail():
+    while True:
+        x = input("Enter your email:")
+        if re.match("[a-zA-Z0-9@.]+$", x):
+            return x
+        else:
+            print("Please enter it again:")
+
 
 def getZcode():
     while True:
@@ -40,15 +66,46 @@ def getZcode():
         except ValueError:
             print("Please enter it again:")
 
+def getCity():
+    while True:
+        x = input("Enter your city:")
+        if re.match("[a-zA-Z]+$", x):
+            return x
+        else:
+            print("Please enter it again:")
+
+def getState():
+    while True:
+        x = input("Enter your state:")
+        if re.match("[a-zA-Z]+$", x):
+            return x
+        else:
+            print("Please enter it again:")
+
+def getRace():
+    while True:
+        x = input("Enter your race:")
+        if re.match("[a-zA-Z]+$", x):
+            return x
+        else:
+            print("Please enter it again:")
+
 def getAge():
     while True:
-        x = input("Enter your zip age:")
+        x = input("Enter your age:")
         try:
             int(x)
             return x
         except ValueError:
             print("Please enter it again:")
 
+def getSex():
+    while True:
+        x = input("Enter your sex:")
+        if re.match("[a-zA-Z]+$", x):
+            return x
+        else:
+            print("Please enter it again:")
 
 
 main()
